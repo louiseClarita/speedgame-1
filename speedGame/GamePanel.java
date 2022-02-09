bc44398b86e1a48438fd6745a54147eefe169700
Clarita Hawat <98291722+louiseClarita@users.noreply.github.com>
2022-02-09 14:27:23 +0200

    Update GamePanel.java

--- speedGame/GamePanel.java
+++ speedGame/GamePanel.java
@@ -1,12 +1,20 @@
 package application;
 
+
+
+import javax.swing.JOptionPane;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.beans.value.WritableValue;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 /*--GamePanel
@@ -14,7 +22,19 @@ import javafx.util.Duration;
 lines*/
 
 public class GamePanel extends Pane {
+	Rectangle Top =new Rectangle();
+
 public GamePanel() {
+	
+	Top.setFill(Color.FIREBRICK);
+	Top.setHeight(20);
+	Top.setWidth(1000);
+	Top.setY(50);
+	Top.setX(50);
+	Top.setStroke(Color.FIREBRICK);
+	getChildren().add(Top);
+	
+	
 	//1st rectangle
 	RectangleShape rectangleShape = new RectangleShape();
 	Line line1 = new Line();
@@ -33,11 +53,14 @@ public GamePanel() {
 	pathTransition.play();
 	pathTransition.setCycleCount(1); 
 
-	KeyFrame keyFrame1 = new KeyFrame(javafx.util.Duration.millis(6000), e -> makeNewRectangle1(line1));
+	KeyFrame keyFrame1 = new KeyFrame(javafx.util.Duration.millis(10000), e -> makeNewRectangle1(line1));
 	Timeline timeLine1 = new Timeline(keyFrame1);
 	timeLine1.setCycleCount(Timeline.INDEFINITE);
 	timeLine1.play();
-	
+	KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.millis(50), e->getCoord(rectangleShape));
+	Timeline animation = new Timeline(keyFrame);
+	animation.setCycleCount(Timeline.INDEFINITE);
+	animation.play(); 
 	
 	// 2nd rectangle
 	RectangleShape rectangleShape2 = new RectangleShape();
@@ -60,7 +83,13 @@ public GamePanel() {
 	pathTransition2.setCycleCount(1);
 	pathTransition2.setAutoReverse(false);
 	pathTransition2.play();
-	KeyFrame keyFrame2 = new KeyFrame(javafx.util.Duration.millis(7000), e -> makeNewRectangle2(line2));
+	
+	KeyFrame keyFrame22 = new KeyFrame(javafx.util.Duration.millis(100), e->getCoord(rectangleShape2));
+	Timeline animation22 = new Timeline(keyFrame22);
+	animation22.setCycleCount(Timeline.INDEFINITE);
+	animation22.play(); 
+	
+	KeyFrame keyFrame2 = new KeyFrame(javafx.util.Duration.millis(6000), e -> makeNewRectangle2(line2));
 	Timeline timeLine2 = new Timeline(keyFrame2);
 	timeLine2.setCycleCount(Timeline.INDEFINITE);
 	timeLine2.play();
@@ -78,7 +107,7 @@ public GamePanel() {
 	getChildren().add(line3);
 	getChildren().add(rectangleShape3.getRectangle());
 
-	KeyFrame keyFrame3 = new KeyFrame(javafx.util.Duration.millis(8000), e -> makeNewRectangle3(line3));
+	KeyFrame keyFrame3 = new KeyFrame(javafx.util.Duration.millis(11000), e -> makeNewRectangle3(line3));
 	Timeline timeLine3 = new Timeline(keyFrame3);
 	timeLine3.setCycleCount(Timeline.INDEFINITE);
 	timeLine3.play();
@@ -90,24 +119,116 @@ public GamePanel() {
 	pathTransition3.setCycleCount(1);
 	pathTransition3.setAutoReverse(false);
 	pathTransition3.play();
-}
-
-private void makeNewRectangle1(Line line) {
-	System.out.println(" NEW rectangle ");
-	RectangleShape rectangleShape12 = new RectangleShape();
-	this.getChildren().add(rectangleShape12.getRectangle());
-	PathTransition pathTransition = new PathTransition();
-	pathTransition.setDuration(Duration.millis(6000));
-	pathTransition.setPath(line);
-	pathTransition.setNode(rectangleShape12.getRectangle());
-	pathTransition.setAutoReverse(false);
-	pathTransition.setCycleCount(1);
-	pathTransition.play();
+	 
+	KeyFrame keyFrame33 = new KeyFrame(javafx.util.Duration.millis(100), e->getCoord(rectangleShape3));
+	Timeline animation33 = new Timeline(keyFrame33);
+	animation33.setCycleCount(Timeline.INDEFINITE);
+	animation33.play(); 
+	
 	
 }
 
-private void makeNewRectangle2(Line line) {
-	System.out.println(" NEW rectangle ");
+private void getCoord(RectangleShape r) {
+  Rectangle rectangle = r.getRectangleOnly();
+ /* Top.setHeight(20);
+	Top.setWidth(1000);
+	Top.setY(0);
+	Top.setX(50);*/
+	if(rectangle.localToParent(getBoundsInLocal()).intersects(Top.localToParent(getBoundsInLocal()))) {
+		//CHECK
+		if(r.getColor() == GameSettings.shapeColor) {
+			 r.checked=true;
+            if(GameSettings.lives == 1) {
+            	/* JOptionPane.showMessageDialog(this, , "Game Over", JOptionPane.YES_NO_OPTION);
+					System.exit(0);
+			 //you have no more lives -> YOU LOSSTTTT configure later 
+			 TryAgain tr = new TryAgain();
+			 Scene sc = tr.getScene();
+			 Main.primaryStage.setScene(sc);
+			 Main.primaryStage.show();*/
+			 
+		}else if(GameSettings.lives > 1) {
+			// you still have some lives left
+			GameSettings.lives--;
+		        }
+		}else if(r.getColor() == GameSettings.shapeColorPressed || r.getColor() == GameSettings.shapeColorhalfPressed) {
+			 r.checked=true;
+			 
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+  /*
+   * 
+   *   
+   *   
+   *   SECOND METHOD :: STILL NOT FINISHED
+   *   
+   *   
+   *   
+   *   Bounds bound = r.localToParent(getBoundsInParent());
+   
+     Double Ycoord = bound.getCenterY();
+    System.out.println("you centerY = " + Ycoord);
+     // you reached top of screen (red line)
+	 if( Ycoord  <= GameSettings.Top && Ycoord > 0) {
+		
+		 // didn't tap required clicks
+		 /*r.Pressed== false && r.HalfPressed == false*/
+		/* if( r.getRectangleOnly().getFill()  == GameSettings.shapeColor) {
+			 System.out.println("NOT PRESSED");
+			 if(GameSettings.lives == 1) {
+				 r.checked=true;
+				 //you have no more lives -> YOU LOSSTTTT configure later 
+				 TryAgain tr = new TryAgain();
+				 Scene sc = tr.getScene();
+				 Main.primaryStage.setScene(sc);
+				 Main.primaryStage.show();
+				 
+				 
+				 
+			 }else if(GameSettings.lives > 1 && r.getRectangleOnly().getFill()  == GameSettings.shapeColor) {
+				 // you lost a life
+				 r.checked=true;
+				 GameSettings.lives = GameSettings.lives - 1; 
+				 System.out.println(" you entered in the lost but not dead");
+				 System.out.println(" lives - 1 = " + GameSettings.lives);
+				 System.out.println("your color color at top line is :" + r.getColor());
+				
+				
+			 }
+			  // you still have one tap or no taps left nothing happens game continues
+			 
+			
+			 }else if( r.getRectangleOnly().getFill() == GameSettings.shapeColorPressed || r.getRectangleOnly().getFill()  == GameSettings.shapeColorhalfPressed) {
+				 r.checked=true;
+				 System.out.println("your color is :" + r.getColor());
+				 System.out.println("PRESSED");
+				 
+			 }
+		
+	 }
+	*/	 
+		 
+
+}
+
+private void makeNewRectangle1(Line line) {
+
 	RectangleShape rectangleShape12 = new RectangleShape();
 	this.getChildren().add(rectangleShape12.getRectangle());
 	PathTransition pathTransition = new PathTransition();
@@ -117,19 +238,70 @@ private void makeNewRectangle2(Line line) {
 	pathTransition.setAutoReverse(false);
 	pathTransition.setCycleCount(1);
 	pathTransition.play();
+	KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.millis(500), e->getCoord(rectangleShape12));
+	Timeline animation = new Timeline(keyFrame);
+	animation.setCycleCount(Timeline.INDEFINITE);
+	animation.play(); 
+	KeyFrame keyFrame1 = new KeyFrame(javafx.util.Duration.millis(1), e->checkIfLost(animation,rectangleShape12));
+	Timeline animation1 = new Timeline(keyFrame1);
+	animation1.setCycleCount(Timeline.INDEFINITE);
+	animation1.play(); 
+	
+
+
 	
 }
-private void makeNewRectangle3(Line line) {
-	System.out.println(" NEW rectangle ");
+
+private void checkIfLost(Timeline animation, RectangleShape r) {
+	if( r.checked == true) {
+		animation.stop();
+	}
+	
+}
+
+private void makeNewRectangle2(Line line) {
+	
 	RectangleShape rectangleShape12 = new RectangleShape();
 	this.getChildren().add(rectangleShape12.getRectangle());
 	PathTransition pathTransition = new PathTransition();
-	pathTransition.setDuration(Duration.millis(8000));
+	pathTransition.setDuration(Duration.millis(4000));
 	pathTransition.setPath(line);
 	pathTransition.setNode(rectangleShape12.getRectangle());
 	pathTransition.setAutoReverse(false);
 	pathTransition.setCycleCount(1);
 	pathTransition.play();
+	KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.millis(50), e->getCoord(rectangleShape12));
+	Timeline animation = new Timeline(keyFrame);
+	animation.setCycleCount(Timeline.INDEFINITE);
+	animation.play(); 
+	KeyFrame keyFrame1 = new KeyFrame(javafx.util.Duration.millis(1), e->checkIfLost(animation,rectangleShape12));
+	Timeline animation1 = new Timeline(keyFrame1);
+	animation1.setCycleCount(Timeline.INDEFINITE);
+	animation1.play();
+
+
+}
+private void makeNewRectangle3(Line line) {
+	
+	RectangleShape rectangleShape12 = new RectangleShape();
+	this.getChildren().add(rectangleShape12.getRectangle());
+	PathTransition pathTransition = new PathTransition();
+	pathTransition.setDuration(Duration.millis(9000));
+	pathTransition.setPath(line);
+	pathTransition.setNode(rectangleShape12.getRectangle());
+	pathTransition.setAutoReverse(false);
+	pathTransition.setCycleCount(1);
+	pathTransition.play();
+	KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.millis(50), e->getCoord(rectangleShape12));
+	Timeline animation = new Timeline(keyFrame);
+	animation.setCycleCount(Timeline.INDEFINITE);
+	animation.play(); 
+	KeyFrame keyFrame1 = new KeyFrame(javafx.util.Duration.millis(1), e->checkIfLost(animation,rectangleShape12));
+	Timeline animation1 = new Timeline(keyFrame1);
+	animation1.setCycleCount(Timeline.INDEFINITE);
+	animation1.play();
 	
 }
+
+
 }
