0da8519eb51bb76164dae1b595e4498043509030
Clarita Hawat <98291722+louiseClarita@users.noreply.github.com>
2022-02-09 14:28:09 +0200

    Update GameSettings.java

--- speedGame/GameSettings.java
+++ speedGame/GameSettings.java
@@ -3,20 +3,23 @@ package application;
 import javafx.scene.paint.Color;
 
 /*--GameSettings (singleton)
-
 static width
 static height
-static  ShapeColor 
-
+static shapeColor
 -score*/
- 
+
 public class GameSettings {
-public static Color shapeColor = Color.BLACK;
+public static Color shapeColor = Color.GREY;
+public static Color shapeColorPressed = Color.DARKOLIVEGREEN;
+public static Color shapeColorhalfPressed = Color.YELLOWGREEN;
 public static final Integer shapeHeight = 200;
 public static final Integer shapeWidth = 100;
 public static Color equationColor = Color.GREEN;
 public static Integer upperBoundRandomNumber = 10;
-
-
+public static Integer score =0;
+public static Integer Top = 296;
+public static Integer lives = 4;
 }
+
+
 
